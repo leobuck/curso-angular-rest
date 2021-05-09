3b3a4e4a283278ab164430dcdbb61d711f410e4c
leobuck <leobuck12@gmail.com>
2021-05-08 22:23:48 -0300

    Adaptador de formatação de data.

--- src/app/components/usuario-add/usuario-add.component.ts
+++ src/app/components/usuario-add/usuario-add.component.ts
@@ -1,9 +1,32 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { Telefone } from 'src/app/model/telefone';
 import { Usuario } from 'src/app/model/usuario';
 import { UsuarioService } from 'src/app/service/usuario.service';
+
+@Injectable()
+export class FormatDateAdapter extends NgbDateAdapter<string> {
+
+  readonly DELIMITER = '/';
+
+  fromModel(value: string | null): NgbDateStruct {
+    if (value) {
+      let date = value.split(this.DELIMITER);
+      return {
+        day: parseInt(date[0], 10),
+        month: parseInt(date[1], 10),
+        year: parseInt(date[2], 10)
+      }
+    }
+    return null;
+  }
+
+  toModel(date: NgbDateStruct | null): string | null {
+    return date ? date.day + this.DELIMITER + date.month + this.DELIMITER + date.year : null;
+  }
+  
+}
 
 @Injectable()
 export class FormataData extends NgbDateParserFormatter {
@@ -43,7 +66,8 @@ export class FormataData extends NgbDateParserFormatter {
   selector: 'app-usuario-add',
   templateUrl: './usuario-add.component.html',
   styleUrls: ['./usuario-add.component.css'],
-  providers: [{ provide: NgbDateParserFormatter, useClass: FormataData }]
+  providers: [{ provide: NgbDateParserFormatter, useClass: FormataData },
+              { provide: NgbDateAdapter, useClass: FormatDateAdapter }]
 })
 export class UsuarioAddComponent implements OnInit {
   usuario = new Usuario();
